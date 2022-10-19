8217e6b7f4f96a3cf761674b8f3b7e5ee2d30b1e
kakutakigen <kigen.collins@gmail.com>
2022-10-19 13:58:14 -0700

    separated numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -prints numbers in order
+ * Return: 0
+ */
+int main(void)
+{
+	int x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
