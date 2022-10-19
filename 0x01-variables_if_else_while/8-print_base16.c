d9d7f99c9858d0171a684f62e6a0bd8874e73bbd
kakutakigen <kigen.collins@gmail.com>
2022-10-19 13:40:15 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -prints hezadecimals
+ * Return: 0
+ */
+int main(void)
+{
+	int num = '0';
+	char letter = 'a';
+
+	while (num <= 9)
+	{
+		putchar(num);
+		num++;
+	}
+	while (letter <= 'f')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
