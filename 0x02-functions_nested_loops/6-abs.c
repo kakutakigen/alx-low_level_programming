60058c397d82d467302e764bf279698e38f2b4df
kakutakigen <kigen.collins@gmail.com>
2022-10-24 05:41:24 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs -prints absolute value
+ * @r: int value to be determined
+ * Return: 0
+ */
+int _abs(int r)
+{
+	int r;
+
+	if (r > 0)
+	{
+		return (r);
+	}
+	else
+	{
+		r *= -1;
+		return (r);
+	}
+}
