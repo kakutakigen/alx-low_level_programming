68dee27d2335560eb3012fa58997f050001a6ac6
kakutakigen <kigen.collins@gmail.com>
2022-10-24 06:56:30 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,16 +14,16 @@ void times_table(void)
 			z = x * y;
 			if ((z / 10) == 0)
 			{
-				if (b == 0)
+				if (y == 0)
 				{
 					_putchar('0');
 				}
-				if (b != 0)
+				if (y != 0)
 				{
 					_putchar(' ');
-					_putchar((c % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-				if (b < 9)
+				if (y < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -31,9 +31,9 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-				if (b < 9)
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+				if (y < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
