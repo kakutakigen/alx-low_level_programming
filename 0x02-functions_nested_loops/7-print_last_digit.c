307e9ef861f123ebd00f94a25f25c844248a1e1b
kakutakigen <kigen.collins@gmail.com>
2022-10-24 06:07:30 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit -prints last digit
+ * @x: int to be determined
+ * Return: 0
+ */
+int print_last_digit(int x)
+{
+	int last_digit = x % 10;
+
+	return (last_digit);
+}
