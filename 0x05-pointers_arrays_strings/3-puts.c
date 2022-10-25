40467f0df178498d7d9946952fa62affb93e5ba6
kakutakigen <kigen.collins@gmail.com>
2022-10-25 16:06:39 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts -prints string followed by new line
+ * @str: string input
+ * Return: 0
+ */
+coid _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
