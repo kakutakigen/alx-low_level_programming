a187198ef6c4651143b3ef8ea8f54e19c63332fc
kakutakigen <kigen.collins@gmail.com>
2022-11-03 02:38:40 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
  * evaluate_num - recursion loop
- * @num: num
+ * @n: number
  * @iterator: integet to iterate
  * Return: 0
  */
-int evaluate_num(int num, int iterator)
+int evaluate_num(int n, int iterator)
 {
-	if (iterator == num - 1)
+	if (iterator == n - 1)
 	{
 		return (1);
 	}
-	else if (num % iterator == 0)
+	else if (n % iterator == 0)
 	{
 		return (0);
 	}
-	if (num % iterator != 0)
+	if (n % iterator != 0)
 	{
-		return (evaluate_num(num, iterator + 1));
+		return (evaluate_num(n, iterator + 1));
 	}
 
 	return (0);
@@ -42,5 +42,5 @@ int is_prime_number(int n)
 		return (1);
 	}
 
-	return (evaluate_num(num, iterator));
+	return (evaluate_num(n, iterator));
 }
