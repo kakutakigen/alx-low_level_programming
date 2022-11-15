8394e8f1d26094147ef4fff0435452ed5f5ba682
kakutakigen <kigen.collins@gmail.com>
2022-11-15 01:39:56 -0800

    initializing dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - initializes dog variable
+ * @d: pointer to struct type dog
+ * @name: pointer to char dog's name
+ * @ age: dog's age
+ * @owner: pointer to char dog's owner
+ * Return: 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
