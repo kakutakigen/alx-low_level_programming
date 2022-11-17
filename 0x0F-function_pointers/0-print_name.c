d4b52cb5e39bf195b5d6e5dfec202846aa349d61
kakutakigen <kigen.collins@gmail.com>
2022-11-17 05:53:53 -0800

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
-#include "function_ponters.h"
+#include <stdlib.h>
 /**
  * print_name - function that prints a name
  * @name: char to display
- * @f: pointer function
- * Return: 0
+ * @f: function pointer
  */
 void print_name(char *name, void (*f)(char *))
 {
