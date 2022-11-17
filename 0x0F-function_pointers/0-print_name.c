1a58c6c8becd58009a0e21cc1a70a744e9e6d6c6
kakutakigen <kigen.collins@gmail.com>
2022-11-17 05:31:36 -0800

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_ponters.h"
+/**
+ * print_name - function that prints a name
+ * @name: char to display
+ * @f: pointer function
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	
+	f(name);
+}
---
+++ 0x0F-function_pointers/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * _putchar - writes char c to stdout
+ * @c: char to print
+ * Return: 0
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
