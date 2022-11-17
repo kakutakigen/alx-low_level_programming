eb9303bf8f94c66d6b24120b95fd5c79e25205c1
kakutakigen <kigen.collins@gmail.com>
2022-11-17 06:14:27 -0800

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - execute function on array via pointer
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function needed
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && size != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(*(array + i));
+		}
+	}
+}
