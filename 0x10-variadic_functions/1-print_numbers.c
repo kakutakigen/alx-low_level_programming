51831b185c0eeebe4e14dd8eebc9e8a14edc2028
kakutakigen <kigen.collins@gmail.com>
2022-12-03 14:33:17 -0800

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,21 +5,24 @@
  * @separator: pointer parameter
  * @n: parameter
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
 
-	va_start(valist, n);
-
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		printf("%d", va_arg(valist, int));
-		if (separator && i < n - 1)
-			printf("%s", separator);
-	}
+		va_start(valist, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(valist, int));
 
+			if (i != n && separator != NULL)
+			{
+				printf("%s", separator);
+			}
+		}
+		va_end(valist);
+	}
 	printf("\n");
-	va_end(valist);
 }
