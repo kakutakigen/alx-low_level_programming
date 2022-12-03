2164fa37aaad99cd5ca6bbc7c82d57e99ec12094
kakutakigen <kigen.collins@gmail.com>
2022-12-03 09:36:59 -0800

    print integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: parameter
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + 0);
+}
