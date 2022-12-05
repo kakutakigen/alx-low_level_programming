96385fab106406bcf54ae8f60b98a089968e8ad9
kakutakigen <kigen.collins@gmail.com>
2022-12-05 12:02:49 -0800

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - prints binary representation of a number
+ * @n: parameter
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int x;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (temp = n, x = 0; (temp >>= 1) > 0; x++)
+		;
+
+	for (; x >= 0; x--)
+	{
+		if ((n >> x) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
